4b8e5b9603fcd58e8c5079db6a3f6fd1d8c3e179
BananaWafflez <benaing12@gmail.com>
2024-01-20 10:49:00 -0500

    Created opeartion functions in js

--- external.js
+++ external.js
@@ -1,3 +1,32 @@
-function operate(num1, num2, operation){
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    if(num2 == 0){
+        return "undefined"
+    }
+    return num1 / num2;
+}
 
+
+
+function operate(num1, num2, operation){
+    switch(operation){
+        case("+"):
+            add(num1,num2);
+            break;
+        case("-"):
+            subtract(num1,num2);
+            break;
+        case("x"):
+            multiply(num1,num2);
+        case("/"):
+            divide(num1,num2);
+    }
 }
