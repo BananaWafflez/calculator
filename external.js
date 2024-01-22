dd606e3d33ec6b6e221c7a2fd5c881aaf3580c30
BananaWafflez <benaing12@gmail.com>
2024-01-22 13:02:24 -0500

    Added 2 number operations in javascript

--- external.js
+++ external.js
@@ -19,15 +19,16 @@ function divide(num1, num2){
 function operate(num1, num2, operation){
     switch(operation){
         case("+"):
-            add(num1,num2);
-            break;
+            return add(num1,num2);
         case("-"):
-            subtract(num1,num2);
-            break;
+            return subtract(num1,num2);
+            
         case("x"):
-            multiply(num1,num2);
+            return multiply(num1,num2);
+            
         case("/"):
-            divide(num1,num2);
+            return divide(num1,num2);
+            
     }
 }
 function changenumberline(numbutton){
@@ -35,7 +36,19 @@ function changenumberline(numbutton){
     if(buttonelem.innerHTML == "+" || buttonelem.innerHTML == "-" || buttonelem.innerHTML == "/" || buttonelem.innerHTML == "*" ){
         num = Number(numberline.innerHTML);
         numarray.push(num);
+        numarray.push(buttonelem.innerHTML);
         numberline.innerHTML = "";
+        if(index < 2){
+            index++;
+        }else{
+            var num1 = numarray[0];
+            var num2 = numarray[2];
+            var operation = numarray[1];
+            console.log(num1);
+            console.log(num2);
+            numberline.innerHTML = operate(num1, num2, operation);
+            
+        }
         return;
     } 
     if(buttonelem.innerHTML == "clear"){
@@ -51,6 +64,7 @@ function changenumberline(numbutton){
 const numberline = document.querySelector(".numberinput");
 const buttons = document.querySelectorAll("button");
 var numarray = []
+var index = 1;
 for(i = 0; i < 15; i++){
     buttons[i].addEventListener('click', changenumberline);
 }
